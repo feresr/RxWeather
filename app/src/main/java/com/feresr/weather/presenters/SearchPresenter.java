6135ac87b3e1d1e4a8594a23ad0e0d860e9e84ac
feresr <fernandoraviola@gmail.com>
2016-08-14 18:22:04 +1000

    pump up version number, update google places API

--- app/src/main/java/com/feresr/weather/presenters/SearchPresenter.java
+++ app/src/main/java/com/feresr/weather/presenters/SearchPresenter.java
@@ -1,7 +1,12 @@
 package com.feresr.weather.presenters;
 
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 
 import com.feresr.weather.UI.FragmentInteractionsListener;
 import com.feresr.weather.UI.RecyclerItemClickListener;
@@ -9,31 +14,54 @@ import com.feresr.weather.UI.SuggestionAdapter;
 import com.feresr.weather.common.BasePresenter;
 import com.feresr.weather.models.City;
 import com.feresr.weather.presenters.views.SearchView;
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.data.DataBufferUtils;
+import com.google.android.gms.location.places.AutocompleteFilter;
+import com.google.android.gms.location.places.AutocompletePrediction;
+import com.google.android.gms.location.places.AutocompletePredictionBuffer;
+import com.google.android.gms.location.places.Places;
 
 import java.util.ArrayList;
 
 import javax.inject.Inject;
 
+
 /**
  * Created by Fernando on 7/11/2015.
  */
-public class SearchPresenter extends BasePresenter<SearchView> implements TextWatcher, RecyclerItemClickListener.OnItemClickListener {
+public class SearchPresenter extends BasePresenter<SearchView> implements TextWatcher, RecyclerItemClickListener.OnItemClickListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     private ArrayList<City> cities;
 
     private GoogleApiClient googleApiClient;
+    private AutocompleteFilter filter;
     private FragmentInteractionsListener fragmentInteractionListener;
     private SuggestionAdapter suggestionAdapter;
+    private PendingResult<AutocompletePredictionBuffer> result;
 
     @Inject
-    public SearchPresenter() {
+    Context context;
+
+    @Inject
+    public SearchPresenter(Context context) {
         super();
+        this.context = context;
     }
 
     @Override
     public void onCreate() {
+        filter = new AutocompleteFilter.Builder()
+                .setTypeFilter(AutocompleteFilter.TYPE_FILTER_CITIES)
+                .build();
         cities = new ArrayList<>();
+        googleApiClient = new GoogleApiClient.Builder(context, this, this)
+                .addApi(Places.GEO_DATA_API)
+                .addApi(Places.PLACE_DETECTION_API)
+                .build();
+        googleApiClient.connect();
     }
 
     @Override
@@ -48,6 +76,31 @@ public class SearchPresenter extends BasePresenter<SearchView> implements TextWa
 
     @Override
     public void onTextChanged(CharSequence s, int start, int before, int count) {
+        if (googleApiClient.isConnected()) {
+            if (result != null) {
+                result.cancel();
+            }
+            result = Places.GeoDataApi.getAutocompletePredictions(googleApiClient, s.toString(),
+                    null, filter);
+
+            result.setResultCallback(new ResultCallback<AutocompletePredictionBuffer>() {
+                @Override
+                public void onResult(@NonNull AutocompletePredictionBuffer autocompletePredictions) {
+                    cities.clear();
+                    for (AutocompletePrediction prediction : autocompletePredictions) {
+                        City city = new City();
+                        city.setName(prediction.getFullText(null).toString());
+                        city.setId(prediction.getPlaceId());
+                        cities.add(city);
+                    }
+                    view.setCities(cities);
+
+                    DataBufferUtils.freezeAndClose(autocompletePredictions);
+                }
+            });
+        } else {
+            Log.d(this.getClass().getSimpleName(), "GoogleApiClient not connected");
+        }
     }
 
     @Override
@@ -74,5 +127,19 @@ public class SearchPresenter extends BasePresenter<SearchView> implements TextWa
 
     public void setFragmentInteractionListener(FragmentInteractionsListener listener) {
         this.fragmentInteractionListener = listener;
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.e(this.getClass().getSimpleName(), connectionResult.getErrorMessage());
     }
 }
